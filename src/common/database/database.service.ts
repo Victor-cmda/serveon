40c7bb2fd5986b15d1faba7f8bb443d72b0603e8
Victor Hugo Somavilla <85908443+Victor-cmda@users.noreply.github.com>
2025-03-27 21:05:59 -0300

    Fix [Database] Resolvendo conexão com banco de dados

--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -13,6 +13,9 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
       user: this.configService.get('DB_USERNAME', 'postgres'),
       password: this.configService.get('DB_PASSWORD', 'postgres'),
       database: this.configService.get('DB_DATABASE', 'pratix'),
+      ssl: {
+        rejectUnauthorized: false // Necessário para conexões com o Render
+      }
     });
   }
 
